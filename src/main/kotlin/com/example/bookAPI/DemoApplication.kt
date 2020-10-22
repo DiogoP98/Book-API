4a9de954af7267652ddc6e0b25d62aadaf3187cd
Diogo Pereira <diogo99p@gmail.com>
2020-10-22 18:07:53 +0100

    buildt data model and sql table

--- src/main/kotlin/com/example/bookAPI/DemoApplication.kt
+++ src/main/kotlin/com/example/bookAPI/DemoApplication.kt
@@ -1,4 +1,4 @@
-package com.example.demo
+package com.example.bookAPI
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
---
+++ src/main/kotlin/com/example/bookAPI/model/Book.kt
@@ -0,0 +1,48 @@
+package com.example.bookAPI.model
+
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "books")
+class Book {
+    //Change Generation value strategy, because Postgres uses sequence tables.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "bookID")
+    val id: Long? = null
+
+    @Column(name = "title")
+    val title: String? = ""
+
+    @Column(name = "authors")
+    val authors: Array<String> = emptyArray()
+
+    @Column(name = "average_rating")
+    val averageRating: Float? = null
+
+    @Column(name = "isbn")
+    val isbn: String? = ""
+
+    @Column(name = "isbn13")
+    val isbn13: String? = ""
+
+    @Column(name = "language_code")
+    val languageCode: String? = ""
+
+    @Column(name = "num_pages")
+    val numPages: Int? = null
+
+    @Column(name = "ratings_count")
+    val ratingsCount: Long? = null
+
+    @Column(name = "text_reviews_count")
+    val textReviewsCount: Long? = null
+
+    @Column(name = "publication_date")
+    val publicationDate: Date? = null
+
+    @Column(name = "publisher")
+    val publisher: String? = ""
+}
